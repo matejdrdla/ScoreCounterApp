21bd6894c3f0303a1190f88ae740f47880f71706
drdlama1 <matej.drdla@uhk.cz>
2016-07-18 10:16:19 +0800

    Testing if that ******* thing is working.

--- app/src/main/java/cz/drdla/matej/scorecounterapp/MainActivity.java
+++ app/src/main/java/cz/drdla/matej/scorecounterapp/MainActivity.java
@@ -27,6 +27,7 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        //THIS IS TEST OF COMMIT AND PUSH
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
